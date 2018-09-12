f21b7e3556c6a6d12746d8d4cd6803baa8fc7ea7
holoyolo88 <holoyolo88@gmail.com>
2018-09-12 19:40:45 +0900

    upload

--- zeroNine/urls.py
+++ zeroNine/urls.py
@@ -5,6 +5,15 @@ from . import views
 urlpatterns = [
 
     #url(r'^$', views.lists, name='lists'),
-    url(r'^$', views.main, name='main'),
+    url(r'^apply_db$', views.main, name='apply_db'),
+    url(r'^apply$', views.apply, name='apply'),
+    url(r'^cancel$', views.cancel, name='cancel'),
+    url(r'^login$', views.login, name='login'),
+    url(r'^main$', views.main, name='main'),
+    url(r'^order$', views.order, name='order'),
+    url(r'^participant$', views.participant, name='participant'),
+    url(r'^productpage_db$', views.productpage_db, name='productpage_db'),
+    url(r'^productpage$', views.productpage, name='productpage'),
+    url(r'^registration$', views.registration, name='registration'),
 ]
 
--- zeroNine/views.py
+++ zeroNine/views.py
@@ -5,5 +5,32 @@ from .models import *
 def lists(request):
     return render(request, 'zeroNine/list.html', {})
 
+def apply_db(request):
+    return render(request, 'zeroNine/apply_db.html', {})
+
+def apply(request):
+    return render(request, 'zeroNine/apply.html', {})
+
+def cancel(request):
+    return render(request, 'zeroNine/cancel.html', {})
+
+def login(request):
+    return render(request, 'zeroNine/login.html', {})
+
 def main(request):
     return render(request, 'zeroNine/main.html', {})
+
+def order(request):
+    return render(request, 'zeroNine/order.html', {})
+
+def participant(request):
+    return render(request, 'zeroNine/participant.html', {})
+
+def productpage_db(request):
+    return render(request, 'zeroNine/productpage_db.html', {})
+
+def productpage(request):
+    return render(request, 'zeroNine/productpage.html', {})
+
+def registration(request):
+    return render(request, 'zeroNine/registration.html', {})
